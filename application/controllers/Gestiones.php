463281831382bdbb3b606f0e89a45413af2609b4
lchaconc <luis.chacon.campos@gmail.com>
2020-06-25 12:05:40 -0600

    imp get y set de tabla diseño

--- application/controllers/Gestiones.php
+++ application/controllers/Gestiones.php
@@ -3,8 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Gestiones extends CI_Controller {
 	
-	public function insertDiseno ()
+	public function setDiseno ()
 	{
-		$this->Gestiones_model->insertDiseno();
+		$this->Gestiones_model->setDiseno();
+	}
+
+	public function getDiseno ()
+	{
+		$result=$this->Gestiones_model->getDiseno();
+		print_r($result);
 	}
 }
--- application/models/Gestiones_model.php
+++ application/models/Gestiones_model.php
@@ -2,15 +2,29 @@
 
 class Gestiones_model extends CI_Model {
 
-public function insertDiseno()
-{
+public function setDiseno() {
     $diseno = array(
             'idUsuario' => 1,  
-            'idProductoDiseno'=> 2, 
-            'descripcion'=> 'Esta es una prueba desde codeigniter' 
+            'idProductoDiseno'=> 5, 
+            'descripcion'=> 'Prueba 852' 
         );
 
-    $this->db->insert('solicitud_diseno', $diseno);
+            $this->db->insert('solicitud_diseno', $diseno);
+}
+
+public function getDiseno()
+{
+   
+    $this->db->select("*");    
+    $this->db->from("solicitud_diseno");    
+    $query=$this->db->get();
+
+    if ($query->num_rows()>0) {
+        return $query->result();
+    } else {
+        return NULL;
+    }    
+    
 }
 
 }
